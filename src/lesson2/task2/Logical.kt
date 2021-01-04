344afd85e69f59971175ee1b67d7ef11e19ed245
DragunOleg <dragun.oleg41@gmail.com>
2021-01-04 15:03:57 +0300

    lesson2 task2 done

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,7 +2,11 @@
 
 package lesson2.task2
 
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.abs
 import lesson1.task1.sqr
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -10,7 +14,7 @@ import lesson1.task1.sqr
  * Лежит ли точка (x, y) внутри окружности с центром в (x0, y0) и радиусом r?
  */
 fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
-    sqr(x - x0) + sqr(y - y0) <= sqr(r)
+    (x - x0) + (y - y0) <= (r)
 
 /**
  * Простая (2 балла)
@@ -18,7 +22,13 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val thousands = (number - number % 1000) / 1000
+    val hundreds = ((number - thousands * 1000) - ((number - thousands * 1000) % 100)) / 100
+    val tens = ((number - thousands * 1000 - hundreds * 100) - ((number - thousands * 1000 - hundreds * 100) % 10)) / 10
+    val ones = number - thousands * 1000 - hundreds * 100 - tens * 10
+    return thousands + hundreds == tens + ones
+}
 
 /**
  * Простая (2 балла)
@@ -27,7 +37,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    (x1 == x2) || (y1 == y2) || (abs(x1 - x2) == abs(y1 - y2))
 
 
 /**
@@ -36,7 +47,15 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int = when {
+    //високосный кратен 400, не кратен 100, кратен 4
+    month == 2 && ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) -> 29
+    month == 2 -> 28
+    month == 4 || month == 6 || month == 9 || month == 11 -> 30
+    else -> 31
+
+}
+
 
 /**
  * Простая (2 балла)
@@ -48,7 +67,8 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = sqrt(sqr(x1 - x2) + sqr(y1 - y2)) + r1 <= r2
+
 
 /**
  * Средняя (3 балла)
@@ -59,4 +79,17 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    //а = наибольшая сторона и будем проверять отверстие для b и c
+    return if (a >= b && a >= c) {
+        max(r, s) >= max(b, c) && min(r, s) >= min(b, c)
+    }
+    //b = наибольшая сторона и будем проверять отверстие для a и c
+    else if (b >= a && b >= c) {
+        max(r, s) >= max(a, c) && min(r, s) >= min(a, c)
+    }
+    //c = наибольшая сторона и будем проверять отверстия для a и b
+    else if (c >= a && c >= b) {
+        max(r, s) >= max(b, a) && min(r, s) >= min(b, a)
+    } else false
+}
